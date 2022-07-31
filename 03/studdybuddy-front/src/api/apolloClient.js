68220288b31fef10bab5c58f5c63b2f7655db864
Hoerrin <patrykd0604@gmail.com>
2022-07-31 21:53:55 +0200

    Finished stage - GraphQL + apollo + datoCMS

---
+++ 03/studdybuddy-front/src/api/apolloClient.js
@@ -0,0 +1,22 @@
+import { ApolloClient, createHttpLink, InMemoryCache} from '@apollo/client';
+import {setContext} from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+    uri: 'https://graphql.datocms.com'
+})
+
+const authLink = setContext((_, { headers }) => {
+    return {
+        headers: {
+            ...headers, 
+            authorization: `Bearer ${DATO_TOKEN}`,
+        }
+    }
+});
+
+export const apolloClient = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+})
+
+const DATO_TOKEN = ''
